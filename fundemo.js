5708b27195ed979d8f81ed0da3dc7fd6d49aad6c
Parthasarathisv <parthasarathisvsr@gmail.com>
2024-10-03 19:38:38 +0530

    Add files via upload

---
+++ fundemo.js
@@ -0,0 +1,6 @@
+function greet(name){
+    console.log('welcome ' + name)
+}
+greet('john')
+greet('jamesh')
+greet('smith')
---
+++ letandvar.js
@@ -0,0 +1,16 @@
+sample()//function calling
+function sample(){//function definition 
+    console.log(x)//accesing variable x
+    var x=10 //declaring and intializing x
+    console.log(x)
+}
+
+//INTERPRETED
+//var x- hoisting (only declaration will be moved to the top of the scope)
+//let console .log(x)
+//x=10
+
+
+// let variable cannot be accessed before initialzing 
+//var variable can be accessed before initialization 
+
