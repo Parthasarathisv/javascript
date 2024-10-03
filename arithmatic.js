cb30e3a7e9da0a3fa09baccafbdd050667ba4fdd
Parthasarathisv <parthasarathisvsr@gmail.com>
2024-10-03 19:34:45 +0530

    arithmatic.js

---
+++ arithmatic.js
@@ -0,0 +1,22 @@
+function add(a,b){
+    return a+b
+}
+function sub (a,b){
+    return a-b
+}
+function multiply (a,b){
+    return a*b
+}
+function divide (a,b){
+return a/b
+}
+
+let sum1=add(10,12);
+let diff=sub(10,12)
+let product =multiply(10,12)
+let quo=divide(10,12)
+
+console.log('sum=' + sum1)
+console.log('diff=' + diff)
+console.log('product='+ product)
+console.log('quotient=' + quo) 
